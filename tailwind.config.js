5442abade5f0bde4fb66ec407d66d1ae19aa3b68
Tether-creator <gruthzillions@gmail.com>
2025-04-05 18:32:03 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#60879d',
+        accent: '#2fb5d5',
+        dark: '#26475e',
+        light: '#b4ccca',
+      },
+    },
+  },
+  plugins: [],
+}
